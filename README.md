ea8ebde891a3e93717b6556a0c002b2f1cad850e
sukohi <eagle.fly.free0722@gmail.com>
2016-03-15 17:45:15 +0900

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # GroupWiseFilter
 A Laravel package to get row(s) with the group-wise maximum or minimum.
+
+(This is for Laravel 5+. [For Laravel 4.2](https://github.com/SUKOHI/GroupWiseFilter/tree/1.0))
+
+# Installation
+
+Execute composer command.
+
+    composer require sukohi/group-wise-filter:2.*
+
+# Preparation
+
+In your model, set GroupWiseFilterTrait.
+
+    use Sukohi\GroupWiseFilter\GroupWiseFilterTrait;
+    
+    class Item extends model
+    {
+        use GroupWiseFilterTrait;
+    }
+
+Now you can call `groupMax()` and `groupMin()`.
+
+# Usage
+
+**Simplest Way**  
+
+    $column = 'amount';
+    $group_by = 'area_id';
+    $items = \App\Item::groupMax($column, $group_by)->get();
+
+**with select(), where() and so on..**  
+
+You should add table name like this.
+
+    $items = \App\Item::select(
+                    'items.id',
+                    'items.title'
+                    'items.area_id'
+                );
+                
+**Grouped table and Grouped table**
+
+You can use `GROUP_TABLE` and `GROUP_VALUE` like this.
+
+		$items = \App\Item::select('GROUP_TABLE.GROUP_VALUE')->orderBy('GROUP_TABLE.GROUP_VALUE', 'ASC');
+
+License
+====
+This package is licensed under the MIT License.
+
+Copyright 2016 Sukohi Kuhoh
---
+++ src/GroupWiseFilterTrait.php
@@ -0,0 +1,67 @@
+<?php namespace Sukohi\GroupWiseFilter;
+
+trait GroupWiseFilterTrait {
+
+	private $_group_wise_group_by,
+			$_group_wise_column;
+
+	public function scopeGroupMax($query, $column, $group_by) {
+
+		$this->groupWiseInit($column, $group_by);
+
+		return $query->join(
+			$this->getGroupSubQuery('max'),
+			$this->getGroupJoin()
+		);
+
+	}
+
+	public function scopeGroupMin($query, $column, $group_by) {
+
+		$this->groupWiseInit($column, $group_by);
+
+		return $query->join(
+			$this->getGroupSubQuery('min'),
+			$this->getGroupJoin()
+		);
+
+	}
+
+	private function groupWiseInit($column, $group_by) {
+
+		$this->_group_wise_column = $column;
+		$this->_group_wise_group_by = $group_by;
+
+	}
+
+	private function getGroupSubQuery($mode) {
+
+		$table = $this->getGroupTable();
+		$group_mode = strtoupper($mode);
+		return \DB::raw(
+			'(SELECT '. $this->_group_wise_group_by .', '. $group_mode .'('. $this->_group_wise_column .') AS GROUP_VALUE '.
+			'FROM '. $table .' '.
+			'GROUP BY '. $this->_group_wise_group_by .') AS GROUP_TABLE'
+		);
+
+	}
+
+	private function getGroupTable() {
+
+		return \DB::getTablePrefix() . $this->getTable();
+
+	}
+
+	private function getGroupJoin() {
+
+		return function($join){
+
+			$table = $this->getGroupTable();
+			$join->on($table .'.'. $this->_group_wise_group_by, '=', 'GROUP_TABLE.'. $this->_group_wise_group_by)
+				->on($table .'.'. $this->_group_wise_column, '=', 'GROUP_TABLE.GROUP_VALUE');
+
+		};
+
+	}
+
+}
